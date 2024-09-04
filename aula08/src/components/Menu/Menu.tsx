da36815dbbd9608e4a5750e4ba71077f54aa3466
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-03 22:52:26 -0300

    Utilizando fetch com promise...

--- aula08/src/components/Menu/Menu.tsx
+++ aula08/src/components/Menu/Menu.tsx
@@ -7,6 +7,7 @@ export default function Menu(){
         <MenuList>
             <MenuItem> <Link to="/">Home</Link></MenuItem>
             <MenuItem><Link to="/produtos">Produtos</Link></MenuItem>
+            <MenuItem><Link to="/users">Usuários</Link></MenuItem>
         </MenuList>
       </Nav>
     );
